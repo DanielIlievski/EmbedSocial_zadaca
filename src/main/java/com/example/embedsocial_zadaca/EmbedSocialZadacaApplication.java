b4911aec18115fa7e44653510ba76f7b3ade244c
DanielIlievski <daniel_101699@hotmail.com>
2022-02-28 02:04:21 +0100

    Initial commit, start of the project

---
+++ src/main/java/com/example/embedsocial_zadaca/EmbedSocialZadacaApplication.java
@@ -0,0 +1,13 @@
+package com.example.embedsocial_zadaca;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmbedSocialZadacaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmbedSocialZadacaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/embedsocial_zadaca/EmbedSocialZadacaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.embedsocial_zadaca;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmbedSocialZadacaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
